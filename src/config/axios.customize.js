0e7b311b2317847203bedca6a47cc916009ff6d7
PTITer2003 <namyahoo2003@gmail.com>
2025-08-17 17:38:52 +0700

    add auth feature

--- src/config/axios.customize.js
+++ src/config/axios.customize.js
@@ -1,5 +1,9 @@
 
 import axios from "axios";
+import { callRefreshToken } from "./api";
+import { store } from "../redux/store";
+import { setRefreshTokenAction } from "../redux/slice/accountSlide";
+import Cookies from "js-cookie";
 const instance = axios.create({
     baseURL: process.env.REACT_APP_BE_URL,
     timeout: 60 * 1000, //60s
@@ -10,7 +14,7 @@ instance.interceptors.request.use(
     async function (config) {
         // Do something before request is sent
         // config.headers["delay"] = 3000;
-        const access_token = JSON.parse(localStorage.getItem('currentUser-agoda'))?.access
+        const access_token = localStorage.getItem('access_token_agoda')
         if (access_token) {
             config.headers["Authorization"] = `Bearer ${access_token}`;
         }
@@ -30,7 +34,33 @@ instance.interceptors.response.use(
         if (response.data) return response.data;
         return response;
     },
-    function (error) {
+    async function (error) {
+
+        if (error.response.status === 401 && error.config.url !== "/api/accounts/login/") {
+            const refresh_token_agoda = Cookies.get("refresh_token_agoda");
+            const res = await callRefreshToken({
+                refresh: refresh_token_agoda
+            });
+            if (res.isSuccess) {
+                error.config.headers[
+                    "Authorization"
+                ] = `Bearer ${res.access}`;
+                localStorage.setItem("access_token_agoda", res.access);
+                return instance.request(error.config);
+            }
+        }
+
+        if (
+            error.response.status === 401 &&
+            error.config.url === "/api/accounts/refresh-token/"
+        ) {
+            const message =
+                error?.response?.data?.message ??
+                "Có lỗi xảy ra, vui lòng login.";
+            //dispatch redux action
+            store.dispatch(setRefreshTokenAction({ status: true, message }));
+        }
+
         if (error?.response?.data) return error?.response?.data;
         // Any status codes that falls outside the range of 2xx cause this function to trigger
         // Do something with response error
