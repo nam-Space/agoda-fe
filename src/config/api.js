a3c40109bfb56171373680aca9fc44e4f0b8881d
PTITer2003 <namyahoo2003@gmail.com>
2025-09-26 08:35:15 +0700

    add airport and location feature

--- src/config/api.js
+++ src/config/api.js
@@ -1,3 +1,4 @@
+import axiosBase from 'axios'
 import axios from "config/axios.customize";
 
 // user
@@ -24,4 +25,64 @@ export const callRefreshToken = (data) => {
 
 export const callLogout = (data) => {
     return axios.post(`/api/accounts/logout/`, { ...data });
+};
+
+// Hotel
+export const callFetchHotel = (query) => {
+    return axios.get(`/api/hotels/hotels/?${query}`);
+};
+
+export const callCreateHotel = (data) => {
+    return axios.post("/api/hotels/hotels/create/", { ...data });
+};
+
+export const callUpdateHotel = (id, data) => {
+    return axios.put(`/api/hotels/hotels/${id}/update/`, { ...data });
+};
+
+export const callDeleteHotel = (id) => {
+    return axios.delete(`/api/hotels/hotels/${id}/delete/`);
+};
+
+export const callDeleteHotelImage = (id) => {
+    return axios.delete(`/api/hotels/hotel-images/${id}/delete/`);
+};
+
+/* Airport */
+export const callFetchAirport = (query) => {
+    return axios.get(`/api/airports/airports/?${query}`);
+};
+
+export const callCreateAirport = (data) => {
+    return axios.post("/api/airports/airports/create/", { ...data });
+};
+
+export const callUpdateAirport = (id, data) => {
+    return axios.put(`/api/airports/airports/${id}/update/`, { ...data });
+};
+
+export const callDeleteAirport = (id) => {
+    return axios.delete(`/api/airports/airports/${id}/delete/`);
+};
+
+// Car
+export const callFetchCar = (query) => {
+    return axios.get(`/api/cars/cars/?${query}`);
+};
+
+export const callCreateCar = (data) => {
+    return axios.post("/api/cars/cars/create/", { ...data });
+};
+
+export const callUpdateCar = (id, data) => {
+    return axios.put(`/api/cars/cars/${id}/update/`, { ...data });
+};
+
+export const callDeleteCar = (id) => {
+    return axios.delete(`/api/cars/cars/${id}/delete/`);
+};
+
+/* Get location Map in all world */
+export const callFetchLocationMapInAllWorld = (query) => {
+    return axiosBase.get(`https://photon.komoot.io/api/?q=${query}`);
 };
