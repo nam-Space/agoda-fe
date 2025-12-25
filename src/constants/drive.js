11b88e1381841878257c1a6cfc38026ea48e6dea
PTITer2003 <namyahoo2003@gmail.com>
2025-12-25 18:28:10 +0700

    fix fetch car api for driver status

---
+++ src/constants/drive.js
@@ -0,0 +1,9 @@
+export const DRIVER_STATUS = {
+    IDLE: "idle",
+    BUSY: "busy",
+};
+
+export const DRIVER_STATUS_VI = {
+    [DRIVER_STATUS.IDLE]: "Đang rảnh",
+    [DRIVER_STATUS.BUSY]: "Bận rộn",
+};
