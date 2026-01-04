76f5bae8609bb9f9d026af946c659fe7d39db039
PTITer2003 <namyahoo2003@gmail.com>
2026-01-04 18:14:59 +0700

    add chatbot module

--- src/layouts/ClientLayout.jsx
+++ src/layouts/ClientLayout.jsx
@@ -1,3 +1,6 @@
+import { CommentOutlined } from "@ant-design/icons";
+import { FloatButton } from "antd";
+import ChatBot from "components/Chatbot/Chatbot";
 import FooterClient from "components/FooterClient";
 import HeaderClient from "components/HeaderClient";
 import React from "react";
@@ -7,6 +10,14 @@ const ClientLayout = ({ children }) => {
         <div>
             <HeaderClient />
             {children}
+            <FloatButton.Group
+                trigger="click"
+                type="primary"
+                style={{ insetInlineEnd: 24 }}
+                icon={<CommentOutlined className="text-[30px]" />}
+            >
+                <ChatBot />
+            </FloatButton.Group>
             <FooterClient />
         </div>
     );
